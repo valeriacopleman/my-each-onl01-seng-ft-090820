5b97058534e3f73b2184ff95c07766d3335cf83a
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-14 17:23:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(array)
+  i = 0 
+words= []
+while i < array.length 
+words<< yield(array[i])
+i +=1 
+   
 end
+return words
+end
+
+
+
+
+
+
+
+ 
+
+
+
